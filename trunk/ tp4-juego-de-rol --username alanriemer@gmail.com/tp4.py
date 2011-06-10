cb3cbf99c57c0f9458032e3d3312b35ddeef5787
maselloleandro@gmail.com <maselloleandro@gmail.com@fa69a240-81c4-5c4e-7ecd-d25bbdb7eacb>
2011-06-10 23:30:38 +0000

    Ahi esta la instancia de la clase!

--- trunk/ tp4-juego-de-rol --username alanriemer@gmail.com/tp4.py
+++ trunk/ tp4-juego-de-rol --username alanriemer@gmail.com/tp4.py
@@ -1,1 +1,15 @@
-El tp parece ser una onda al ejercicio ese de herencia que teniamos en la guia...
+import random
+class Criatura(object):
+	"""Objeto donde se encuentran los atributos y habilidades de las criaturas"""
+	def __init__(self):
+		"""Metodo constructor de la clase. Crea la instancia de la clase de las criaturas.
+		precondiciones: Las 6 caracteristicas de la criatura deben ser numeros naturales, con el 0 incluido"""
+		self.caracteristicas={"fuerza":0,"inteligencia":0,"contextura":0, "destreza":0, "carisma":0,"sabiduria":0}
+		for clave in self.caracteristicas.keys():
+                    tirar_dados=random.randrange(1,7)+random.randrange(1,7)+random.randrange(1,7)
+                    self.caracteristicas[clave]=tirar_dados
+		suma_caracteristicas=0
+		for valor in self.caracteristicas.values():
+                    suma_caracteristicas+=valor
+		self.indicadores={"hp":suma_caracteristicas,"mp":self.caracteristicas["inteligencia"]+self.caracteristicas["sabiduria"],"xp":0}
+	
