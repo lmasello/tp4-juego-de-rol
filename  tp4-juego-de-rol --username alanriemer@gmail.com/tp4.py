d7f15ce3ca12c906636939e1dc16dc610c557031
maselloleandro@gmail.com <maselloleandro@gmail.com@fa69a240-81c4-5c4e-7ecd-d25bbdb7eacb>
2011-06-14 05:51:08 +0000

    modifique unas cositas de las habilidades y le agregue huir a goku. Aunque cacaroto nunca huye :P

---  tp4-juego-de-rol --username alanriemer@gmail.com/tp4.py
+++  tp4-juego-de-rol --username alanriemer@gmail.com/tp4.py
@@ -51,7 +51,7 @@ class Golpe_martillo(object):
         def obtener_costos(self):
                 """Devuelve un diccionario con los valores mnimos de caractersticas
 y/o indicadores que debe tener la criatura para poder aplicar la habilidad."""
-                costos={"fuerza":14,"inteligencia":4, "contextura":13, "destreza":12, "carisma":4, "sabiduria":3}
+                costos={"fuerza":8, "contextura":5, "destreza":6}
                 return costos
 
         def obtener_consecuencias(self, origen, final):
@@ -76,13 +76,13 @@ y/o indicadores que debe tener la criatura para poder aplicar la habilidad."""
 class Ataque_wachenhausen(objecto):
         """Modela la habilidad"""
 	def __init__(self):
-                self.nombre="Wachenhausen"
-                self.descripcion="Te quema la cabeza"
+                self.nombre="Ataque_wachenhausen"
+                self.descripcion="Utilizado por aquellas criaturas con un grado de inteligencia notable. El Ataque_wachenhausen incorpora a la criatura una determiada inteligencia y la posibilidad de usar un ataque utilizado con la telepatia para dañar al oponente"
                 self.autor="Riemer-Masello"
         def obtenercostos(self):
                 """Devuelve un diccionario con los valores mnimos de caractersticas
 y/o indicadores que debe tener la criatura para poder aplicar la habilidad."""
-                costos={"fuerza":4,"inteligencia":16, "contextura":5, "destreza":14, "carisma":16, "sabiduria":16, "mp":10}
+                costos={"inteligencia":7, "destreza":6, "carisma":6, "sabiduria":7, "mp":10}
 		return costos
 	def obtener_consecuencias(self, origen, final):
                 """Devuelve dos diccionarios con las alteraciones a realizar sobre los atributos de la criatura origen y la criatura destino
@@ -98,18 +98,18 @@ y/o indicadores que debe tener la criatura para poder aplicar la habilidad."""
                             return  modificaciones_criatura_origen, modificaciones_criatura_destino   #Devuelve los diccionarios con dichos valores en 0
         #Si la criatura cumple con los requisitos necesarios, se procede
                 modificaciones_criatura_destino={"fuerza":0,"inteligencia":0,"contextura":-1, "destreza":-1, "carisma":-1,"sabiduria":0,"xp":2,"hp":-35,"mp":0}
-                modificaciones_criatura_origen={"fuerza":0,"inteligencia":0,"contextura":0, "destreza":0, "carisma":0,"sabiduria":0,"xp":2,"hp":0,"mp":-10}   
+                modificaciones_criatura_origen={"fuerza":0,"inteligencia":6,"contextura":0, "destreza":0, "carisma":0,"sabiduria":3,"xp":2,"hp":0,"mp":10}   
                 return modificaciones_criatura_origen, modificaciones_criatura_destino
 class Fatality(objecto):
         """Modela la habilidad"""
 	def __init__(self):
                 self.nombre="Fatality"
-                self.descripcion="Te mata"
+                self.descripcion="Fatality es una habilidad que solo podran acceder aquellas criaturas que posean un grado de magia y de experiencia equivalente a 40. Ya que es u ataque que requiere de una energia muy elevada. Y solo aquellas criaturas que logren obtener ea cantidad de puntos podran utilizarla"
                 self.autor="Riemer-Masello"
         def obtenercostos(self):
                 """Devuelve un diccionario con los valores mnimos de caractersticas
 y/o indicadores que debe tener la criatura para poder aplicar la habilidad."""
-                costos={"fuerza":13,"inteligencia":16, "contextura":13, "destreza":14, "carisma":16, "sabiduria":16, "mp":40}
+                costos={"mp":40,"xp":40}
 		return costos
 	def obtener_consecuencias(self, origen, final):
                 """Devuelve dos diccionarios con las alteraciones a realizar sobre los atributos de la criatura origen y la criatura destino
@@ -124,8 +124,8 @@ y/o indicadores que debe tener la criatura para poder aplicar la habilidad."""
                         if valores_minimos[clave]>atributos_criatura[clave]:      #Caso en que los atributos de la criatura no alcanzan para realizar la habilidad
                             return  modificaciones_criatura_origen, modificaciones_criatura_destino   #Devuelve los diccionarios con dichos valores en 0
         #Si la criatura cumple con los requisitos necesarios, se procede
-                modificaciones_criatura_destino={"fuerza":0,"inteligencia":0,"contextura":-1, "destreza":-1, "carisma":-1,"sabiduria":0,"xp":2,"hp":-80,"mp":0}
-                modificaciones_criatura_origen={"fuerza":0,"inteligencia":0,"contextura":0, "destreza":0, "carisma":0,"sabiduria":0,"xp":2,"hp":0,"mp":-40}   
+                modificaciones_criatura_destino={"fuerza":-6,"inteligencia":-6,"contextura":-6, "destreza":-6, "carisma":-6,"sabiduria":-6,"xp":2,"hp":-80,"mp":0}
+                modificaciones_criatura_origen={"fuerza":0,"inteligencia":0,"contextura":0, "destreza":0, "carisma":0,"sabiduria":0,"xp":6,"hp":0,"mp":-40}   
                 return modificaciones_criatura_origen, modificaciones_criatura_destino
                 
 class Big_Bang_Attack(object):
@@ -152,8 +152,8 @@ class Big_Bang_Attack(object):
                 if valores_minimos[clave]>atributos_criatura[clave]:      #Caso en que los atributos de la criatura no alcanzan para realizar la habilidad
                     return  modificaciones_criatura_origen, modificaciones_criatura_destino   #Devuelve los diccionarios con dichos valores en 0
         #Si la criatura cumple con los requisitos necesarios, se procede
-        modificaciones_criatura_destino={"fuerza":0,"inteligencia":0,"contextura":-1, "destreza":-1, "carisma":-1,"sabiduria":0,"xp":0,"hp":-35,"mp":0}
-        modificaciones_criatura_origen={"fuerza":0,"inteligencia":0,"contextura":0, "destreza":0, "carisma":0,"sabiduria":0,"xp":2,"hp":0,"mp":-10}   
+        modificaciones_criatura_destino={"fuerza":0,"inteligencia":0,"contextura":-1, "destreza":-1, "carisma":-1,"sabiduria":0,"xp":1,"hp":-25,"mp":0}
+        modificaciones_criatura_origen={"fuerza":0,"inteligencia":0,"contextura":0, "destreza":0, "carisma":0,"sabiduria":0,"xp":2,"hp":0,"mp":-2}   
         return modificaciones_criatura_origen, modificaciones_criatura_destino
 
 class Psicoataque(object):
@@ -220,7 +220,7 @@ def __init__(self):
         self.autor="Masello-Riemer"
     def obtener_costos(self):
         """Devuelve un diccionario con los valores minimos de caracteristicas y/o indicadores que debe tener la criatura para poder aplicar la habilidad"""
-        valores_minimos={"sabiduria":5,"xp":2,"mp":8}
+        valores_minimos={"sabiduria":5,"xp":6,"mp":8}
         return valores_minimos
     def obtener_consecuencias(self,origen,destino):
         """Devuelve dos diccionarios con las alteraciones a realizar sobre los atributos de la criatura origen y la criatura destino
@@ -235,7 +235,7 @@ def __init__(self):
                 if valores_minimos[clave]>atributos_criatura[clave]:      #Caso en que los atributos de la criatura no alcanzan para realizar la habilidad
                     return  modificaciones_criatura_origen, modificaciones_criatura_destino   #Devuelve los diccionarios con dichos valores en 0
         #Si la criatura cumple con los requisitos necesarios, se procede
-        modificaciones_criatura_destino={"fuerza":-5,"inteligencia":0,"contextura":-1, "destreza":0, "carisma":0,"sabiduria":0,"xp":1,"hp":-19,"mp":0}
+        modificaciones_criatura_destino={"fuerza":-5,"inteligencia":0,"contextura":-1, "destreza":0, "carisma":0,"sabiduria":0,"xp":1,"hp":-25,"mp":0}
         modificaciones_criatura_origen={"fuerza":0,"inteligencia":0,"contextura":0, "destreza":0, "carisma":0,"sabiduria":5,"xp":3,"hp":0,"mp":-1}   
         return modificaciones_criatura_origen, modificaciones_criatura_destino
 class Posion(object):
@@ -424,6 +424,10 @@ class Jugador_artificial_2(object):
    def elegir_accion(self,origen, destino):
         """Devuelve el nombre de la habilidad a utilizar y la criatura destino de la habilidad. En el caso de ser un jugador real, interactua con
 el usuario para decidirlo, en el caso de ser un jugador artificial, evalua programaticamente sus posibilidades y elige una."""
+        valor_hp=indicadores_de_criatura["hp"]
+            #Si la vida es menor a 15 le permite huir
+        if valor_hp<15:
+          return "huir", destino      #Verificar lo que devuelve, si es que devuelve "huir" o podria devolver None
         habilidades_criatura=origen.obtenerhabilidades() #Diccionario con habilidades de criatura origen
         posibles_habilidades=habilidades_criatura.keys() #Lista con nombres de habilidades a usar
         habilidad_elegida=None
